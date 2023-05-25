51fcd51731066084cd90882dc73caf62d9a67c33
Salavat Nigmatullin <salavat@nigmatullin.net>
2023-05-25 10:46:08 +0300

    Libraries

---
+++ Libraries/Faker/demo.py
@@ -0,0 +1,19 @@
+from faker import Faker
+
+fake = Faker()
+
+
+first_name  = fake.first_name()
+last_name   = fake.last_name()
+balance     = fake.random_int(11, 243589)
+birthday  = fake.date_of_birth().strftime('%Y-%m-%d')
+email       = fake.company_email()
+
+data = {
+    'firstname': first_name, 
+    'lastname': last_name, 
+    'balance': balance, 
+    'birth_date': birthday, 
+    'email': email}
+
+print(data)
