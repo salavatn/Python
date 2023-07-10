5d365a33a56bd0f508fa6d34112a6cb86e67a9f0
Salavat Nigmatullin <salavat@nigmatullin.net>
2023-07-10 13:48:16 +0300

    DJANGO ORM

--- SQLAlchemy/sql_create_table.py
+++
@@ -1,7 +0,0 @@
-print("\nFILE START: SQL_CREATE_TABLE")
-from sql_connection import base, engine
-from sql_table_list import TableCustomers
-
-table = TableCustomers
-base.metadata.create_all(engine)
-print("\tFILE FINISH: SQL_CREATE_TABLE\n")
